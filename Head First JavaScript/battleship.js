d58238fe08c854e3aea537924a634a616928307d
Sergey Iryupin <biblelamp@yandex.ru>
2016-07-04 14:27:19 +0300

    chapter 2. simple battleship improved version

--- Head First JavaScript/battleship.js
+++ Head First JavaScript/battleship.js
@@ -1,19 +1,20 @@
-var location1 = 3;
-var location2 = 4;
-var location3 = 5;
+var randomLoc = Math.floor(Math.random() * 5);
+var location1 = randomLoc;
+var location2 = location1 + 1;
+var location3 = location2 + 1
 var guess;
 var hits = 0;
 var guesses = 0;
 var isSunk = false;
-while (isSunk ==false) { 
+while (!isSunk) { 
     guess = prompt("Ready, aim, fire! (enter a number from 0-6):");
     if (guess < 0 || guess > 6) { 
         alert("Please enter a valid cell number!");
     } else { 
-        guesses = guesses + 1;
-        if (guess ==location1 || guess ==location2 || guess ==location3) {
+        guesses++;
+        if (guess == location1 || guess == location2 || guess == location3) {
             alert("HIT!");
-            hits = hits + 1;
+            hits++;
             if (hits == 3) {
                 isSunk = true;
                 alert("You sank my battleship!");

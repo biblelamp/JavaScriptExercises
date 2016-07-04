a44fc5f8634bfa252cd41a1a5c3f1cde33d2e5e0
Sergey Iryupin <biblelamp@yandex.ru>
2016-07-04 10:34:12 +0300

    lesson 1. homework

---
+++ JavaScript 1/lesson1.js
@@ -0,0 +1,4 @@
+var admin; // to declare variable
+var name = 'Vasiliy'; // to declare and initialize a variable
+admin = name; // to assigns the value of one variable to another variable
+alert("Admin: " + admin); // to show the value of the variable

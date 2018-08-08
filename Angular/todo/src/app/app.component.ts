f069f195049f75809b58c225dccd9093a921350c
Sergey Iryupin <biblelamp@yandex.ru>
2018-08-08 10:40:43 +0200

    app works in real time

--- Angular/todo/src/app/app.component.ts
+++ Angular/todo/src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { Model, TodoItem } from './model';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'todo-app',
+  templateUrl: './app.component.html'
 })
+
 export class AppComponent {
-  title = 'todo';
-}
+  model = new Model();
+
+  getName() {
+    return this.model.user;
+  }
+
+  getTodoItems() {
+    return this.model.items.filter(item => !item.done);
+  }
+
+  addItem(newItem) {
+    if (newItem != "") {
+      this.model.items.push(new TodoItem(newItem, false));
+    }
+  }}

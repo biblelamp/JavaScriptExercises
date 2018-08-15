7fd2cdb723f8096e3ecb0935ea6a0aadcb1e70e9
Sergey Iryupin <biblelamp@yandex.ru>
2018-08-15 12:39:30 +0200

    changed root module

--- Angular/SportStore/src/app/app.component.ts
+++ Angular/SportStore/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app',
+  template: `<div class="bg-success p-a-1 text-xs-center">
+                This is SportsStore
+            </div>`
 })
-export class AppComponent {
-  title = 'SportStore';
-}
+export class AppComponent { }

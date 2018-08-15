988ca3fd815eec9ca3467eca591f53006b58a6b2
Sergey Iryupin <biblelamp@yandex.ru>
2018-08-15 14:18:19 +0200

    included store module

--- Angular/SportStore/src/app/app.component.ts
+++ Angular/SportStore/src/app/app.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app',
-  template: `<div class="bg-success p-a-1 text-xs-center">
-                This is SportsStore
-            </div>`
+  template: '<store></store>'
 })
 export class AppComponent { }
--- Angular/SportStore/src/app/app.module.ts
+++ Angular/SportStore/src/app/app.module.ts
@@ -2,14 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StoreModule } from "./store/store.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, StoreModule],
   providers: [],
   bootstrap: [AppComponent]
 })

b44220be02f0fd3b919767c2260d89bc32bc6d2a
Sergey Iryupin <biblelamp@yandex.ru>
2018-08-16 09:10:28 +0200

    information about products by category

--- Angular/SportStore/src/app/store/store.component.ts
+++ Angular/SportStore/src/app/store/store.component.ts
@@ -4,19 +4,24 @@ import { ProductRepository } from "../model/product.repository";
 
 @Component({
   selector: "store",
-  //moduleId: module()
+  //moduleId: module.id,
   templateUrl: "store.component.html"
 })
 export class StoreComponent {
+  public selectedCategory = null;
 
   constructor(private repository: ProductRepository) {
   }
 
   get products(): Product[] {
-    return this.repository.getProducts();
+    return this.repository.getProducts(this.selectedCategory);
   }
 
   get categories(): string[] {
     return this.repository.getCategories();
+  }
+
+  changeCategory(newCategory?: string) {
+    this.selectedCategory = newCategory;
   }
 }

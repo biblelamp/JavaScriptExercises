17cbdb96efa501143f4a111c7c8f58f5d3f82e24
Sergey Iryupin <biblelamp@yandex.ru>
2018-08-17 14:02:53 +0200

    added route guard

--- Angular/SportStore/src/app/app.module.ts
+++ Angular/SportStore/src/app/app.module.ts
@@ -3,19 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { StoreModule } from "./store/store.module";
-import {RouterModule} from "@angular/router";
-import {StoreComponent} from "./store/store.component";
-import {CartDetailComponent} from "./store/cartDetail.component";
-import {CheckoutComponent} from "./store/checkout.component";
+import { RouterModule } from "@angular/router";
+import { StoreComponent } from "./store/store.component";
+import { CartDetailComponent } from "./store/cartDetail.component";
+import { CheckoutComponent } from "./store/checkout.component";
+import { StoreFirstGuard } from "./storeFirst.guard";
 
 @NgModule({
   imports: [BrowserModule, StoreModule,
     RouterModule.forRoot([
-      { path: "store", component: StoreComponent },
-      { path: "cart", component: CartDetailComponent },
-      { path: "checkout", component: CheckoutComponent },
+      { path: "store", component: StoreComponent,
+        canActivate: [StoreFirstGuard] },
+      { path: "cart", component: CartDetailComponent,
+        canActivate: [StoreFirstGuard] },
+      { path: "checkout", component: CheckoutComponent,
+        canActivate: [StoreFirstGuard] },
       { path: "**", redirectTo: "/store" }
     ])],
+  providers: [StoreFirstGuard],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })

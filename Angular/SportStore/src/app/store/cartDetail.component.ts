d8d38c3fc34c57505a047873eb7f60f0c0e43cc2
Sergey Iryupin <biblelamp@yandex.ru>
2018-08-17 14:15:53 +0200

    added cart realization

--- Angular/SportStore/src/app/store/cartDetail.component.ts
+++ Angular/SportStore/src/app/store/cartDetail.component.ts
@@ -1,6 +1,10 @@
 import { Component } from "@angular/core";
+import { Cart } from "../model/cart.model";
 
 @Component({
-  template: `<div><h3 class="bg-info p-a-1">Cart Detail Component</h3></div>`
+  //moduleId: module.id,
+  templateUrl: "cartDetail.component.html"
 })
-export class CartDetailComponent { }
+export class CartDetailComponent {
+  constructor(public cart: Cart) { }
+}

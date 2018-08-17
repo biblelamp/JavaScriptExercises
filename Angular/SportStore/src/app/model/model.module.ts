213d04e71f3408918d1cc071f4520c169121b319
Sergey Iryupin <biblelamp@yandex.ru>
2018-08-17 14:58:16 +0200

    updated repository and data source

--- Angular/SportStore/src/app/model/model.module.ts
+++ Angular/SportStore/src/app/model/model.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from "@angular/core";
 import { ProductRepository } from "./product.repository";
 import { StaticDataSource } from "./static.datasource";
 import { Cart } from "./cart.model";
+import { Order } from "./order.model";
+import { OrderRepository } from "./order.repository";
 
 @NgModule({
-  providers: [ProductRepository, StaticDataSource, Cart]
+  providers: [ProductRepository, StaticDataSource, Cart,
+        Order, OrderRepository]
 })
 export class ModelModule { }

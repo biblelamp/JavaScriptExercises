dc1706888fc88ec7421dc8319a0d3dfb4d44cc53
Sergey Iryupin <biblelamp@yandex.ru>
2018-08-20 08:16:58 +0200

    completion of the order

--- Angular/SportStore/src/app/store/checkout.component.ts
+++ Angular/SportStore/src/app/store/checkout.component.ts
@@ -1,6 +1,28 @@
 import { Component } from "@angular/core";
+import {OrderRepository} from "../model/order.repository";
+import {Order} from "../model/order.model";
+import {NgForm} from "@angular/forms";
 
 @Component({
-  template: `<div><h3 class="bg-info p-a-1">Checkout Component</h3></div>`
+  //moduleId: module.id,
+  templateUrl: "checkout.component.html",
+  styleUrls: ["checkout.component.css"]
 })
-export class CheckoutComponent { }
+export class CheckoutComponent {
+  orderSent: boolean = false;
+  submitted: boolean = false;
+
+  constructor(public repository: OrderRepository,
+              public order: Order) { }
+
+  submitOrder(form: NgForm) {
+    this.submitted = true;
+    if (form.valid) {
+      this.repository.saveOrder(this.order).subscribe(order => {
+        this.order.clear();
+        this.orderSent = true;
+        this.submitted = false;
+      });
+    }
+  }
+}

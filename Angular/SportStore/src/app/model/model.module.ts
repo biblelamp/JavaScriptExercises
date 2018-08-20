bb428b1ba8bdf1a0e20101ab373c5ba85ae1842e
Sergey Iryupin <biblelamp@yandex.ru>
2018-08-20 08:55:50 +0200

    switching to an external data source

--- Angular/SportStore/src/app/model/model.module.ts
+++ Angular/SportStore/src/app/model/model.module.ts
@@ -4,9 +4,12 @@ import { StaticDataSource } from "./static.datasource";
 import { Cart } from "./cart.model";
 import { Order } from "./order.model";
 import { OrderRepository } from "./order.repository";
+import { HttpModule } from "@angular/http";
+import { RestDataSource } from "./rest.datasource";
 
 @NgModule({
-  providers: [ProductRepository, StaticDataSource, Cart,
-        Order, OrderRepository]
+  imports: [HttpModule],
+  providers: [ProductRepository, Cart, Order, OrderRepository,
+    { provide: StaticDataSource, useClass: RestDataSource }]
 })
 export class ModelModule { }

3023b07e5ad2bb15a498466432cda7936c4bb6fd
Sergey Iryupin <biblelamp@yandex.ru>
2018-08-22 12:31:35 +0200

    added authentication

--- Angular/SportStore/src/app/model/rest.datasource.ts
+++ Angular/SportStore/src/app/model/rest.datasource.ts
@@ -12,25 +12,44 @@ const PORT = 3500;
 @Injectable()
 export class RestDataSource {
   baseUrl: string;
+  auth_token: string;
 
   constructor(private http: Http) {
     this.baseUrl = `${PROTOCOL}://${location.hostname}:${PORT}/`;
   }
 
-  getProducts(): Observable<Product> {
+  authenticate(user: string, pass: string): Observable<boolean> {
+    return this.http.request(new Request({
+      method: RequestMethod.Post,
+      url: this.baseUrl + "login",
+      body: { name: user, password: pass }
+    })).map(response => {
+      let r = response.json();
+      this.auth_token = r.success ? r.token : null;
+      return r.success;
+    });
+  }
+
+  getProducts(): Observable<Product | Product[]> {
     return this.sendRequest(RequestMethod.Get, "products");
   }
 
-  saveOrder(order: Order): Observable<Product | Order> {
+  saveOrder(order: Order): Observable<Product | Product[] | Order | Order[]> {
     return this.sendRequest(RequestMethod.Post, "orders", order);
   }
 
   private sendRequest(verb: RequestMethod,
-                      url: string, body?: Product | Order): Observable<Product | Order> {
-    return this.http.request(new Request({
+                      url: string, body?: Product | Order, auth: boolean = false)
+                          : Observable<Product | Product[] | Order | Order[]> {
+
+    let request = new Request({
       method: verb,
       url: this.baseUrl + url,
       body: body
-    })).map(response => response.json());
+    });
+    if (auth && this.auth_token != null) {
+      request.headers.set("Authorization", `Bearer<${this.auth_token}>`);
+    }
+    return this.http.request(request).map(response => response.json());
   }
 }

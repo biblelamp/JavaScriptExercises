f43e9ce37494e045d804b198ba20b30cd3937747
Sergey Iryupin <biblelamp@yandex.ru>
2018-08-30 16:25:08 +0200

    added new operations

--- Angular/SportStore/src/app/model/rest.datasource.ts
+++ Angular/SportStore/src/app/model/rest.datasource.ts
@@ -34,6 +34,36 @@ export class RestDataSource {
     return this.sendRequest(RequestMethod.Get, "products");
   }
 
+  saveProduct(product: Product): Observable<Product | Product[]> {
+    return this.sendRequest(RequestMethod.Post, "products",
+      product, true);
+  }
+
+  updateProduct(product): Observable<Product | Product[]> {
+    return this.sendRequest(RequestMethod.Put,
+      `products/${product.id}`, product, true);
+  }
+
+  deleteProduct(id: number): Observable<Product | Product[]> {
+    return this.sendRequest(RequestMethod.Delete,
+      `products/${id}`, null, true);
+  }
+
+  getOrders(): Observable<Product | Product[] | Order | Order[]> {
+    return this.sendRequest(RequestMethod.Get,
+      "orders", null, true);
+  }
+
+  deleteOrder(id: number): Observable<Product | Product[] | Order | Order[]> {
+    return this.sendRequest(RequestMethod.Delete,
+      `orders/${id}`, null, true);
+  }
+
+  updateOrder(order: Order): Observable<Product | Product[] | Order | Order[]> {
+    return this.sendRequest(RequestMethod.Put,
+      `orders/${order.id}`, order, true);
+  }
+
   saveOrder(order: Order): Observable<Product | Product[] | Order | Order[]> {
     return this.sendRequest(RequestMethod.Post, "orders", order);
   }

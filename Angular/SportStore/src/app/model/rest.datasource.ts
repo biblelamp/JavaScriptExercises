d291426da755813f211d93e281b6edeefadca121
Sergey Iryupin <biblelamp@yandex.ru>
2018-09-06 12:46:03 +0200

    fixed accodring update from repo

--- Angular/SportStore/src/app/model/rest.datasource.ts
+++ Angular/SportStore/src/app/model/rest.datasource.ts
@@ -30,47 +30,48 @@ export class RestDataSource {
     });
   }
 
-  getProducts(): Observable<Product | Product[]> {
-    return this.sendRequest(RequestMethod.Get, "products");
+  getProducts(): Observable<Product[]> {
+    return this.sendRequest(RequestMethod.Get, "products") as Observable<Product[]>;
   }
 
-  saveProduct(product: Product): Observable<Product | Product[]> {
+  saveProduct(product: Product): Observable<Product> {
     return this.sendRequest(RequestMethod.Post, "products",
-      product, true);
+      product, true) as Observable<Product>;
   }
 
-  updateProduct(product): Observable<Product | Product[]> {
+  updateProduct(product): Observable<Product> {
     return this.sendRequest(RequestMethod.Put,
-      `products/${product.id}`, product, true);
+      `products/${product.id}`, product, true) as Observable<Product>;
   }
 
-  deleteProduct(id: number): Observable<Product | Product[]> {
+  deleteProduct(id: number): Observable<Product> {
     return this.sendRequest(RequestMethod.Delete,
-      `products/${id}`, null, true);
+      `products/${id}`, null, true) as Observable<Product>;
   }
 
-  getOrders(): Observable<Product | Product[] | Order | Order[]> {
+  getOrders(): Observable<Order[]> {
     return this.sendRequest(RequestMethod.Get,
-      "orders", null, true);
+      "orders", null, true) as Observable<Order[]>;
   }
 
-  deleteOrder(id: number): Observable<Product | Product[] | Order | Order[]> {
+  deleteOrder(id: number): Observable<Order> {
     return this.sendRequest(RequestMethod.Delete,
-      `orders/${id}`, null, true);
+      `orders/${id}`, null, true) as Observable<Order>;
   }
 
-  updateOrder(order: Order): Observable<Product | Product[] | Order | Order[]> {
+  updateOrder(order: Order): Observable<Order> {
     return this.sendRequest(RequestMethod.Put,
-      `orders/${order.id}`, order, true);
+      `orders/${order.id}`, order, true) as Observable<Order>;
   }
 
-  saveOrder(order: Order): Observable<Product | Product[] | Order | Order[]> {
-    return this.sendRequest(RequestMethod.Post, "orders", order);
+  saveOrder(order: Order): Observable<Order> {
+    return this.sendRequest(RequestMethod.Post,
+      "orders", order) as Observable<Order>;
   }
 
   private sendRequest(verb: RequestMethod,
                       url: string, body?: Product | Order, auth: boolean = false)
-                          : Observable<Product | Product[] | Order | Order[]> {
+    : Observable<Product | Product[] | Order | Order[]> {
 
     let request = new Request({
       method: verb,

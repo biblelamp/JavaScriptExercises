00a87e9b0fe34277256e555ade95bd49b8b618b6
Sergey Iryupin <biblelamp@yandex.ru>
2018-09-07 13:58:16 +0200

    added table of products

--- Angular/SportStore/src/app/admin/productTable.component.ts
+++ Angular/SportStore/src/app/admin/productTable.component.ts
@@ -1,8 +1,19 @@
 import { Component } from "@angular/core";
+import { Product } from "../model/product.model";
+import { ProductRepository } from "../model/product.repository";
 
 @Component({
-  template: `<div class="bg-info p-a-1">
-    <h3>Product Table Placeholder</h3>
-  </div>`
+  //moduleId: module.id,
+  templateUrl: "productTable.component.html"
 })
-export class ProductTableComponent { }
+export class ProductTableComponent {
+  constructor(private repository: ProductRepository) { }
+
+  getProducts(): Product[] {
+    return this.repository.getProducts();
+  }
+
+  deleteProduct(id: number) {
+    this.repository.deleteProduct(id);
+  }
+}

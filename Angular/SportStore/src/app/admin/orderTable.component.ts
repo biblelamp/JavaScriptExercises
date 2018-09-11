f3c7e45d2f16dab3628631dcff50e73a543998d8
Sergey Iryupin <biblelamp@yandex.ru>
2018-09-11 08:03:52 +0200

    added order management

--- Angular/SportStore/src/app/admin/orderTable.component.ts
+++ Angular/SportStore/src/app/admin/orderTable.component.ts
@@ -1,8 +1,27 @@
 import { Component } from "@angular/core";
+import { Order } from "../model/order.model";
+import { OrderRepository } from "../model/order.repository";
 
 @Component({
-  template: `<div class="bg-primary p-a-1">
-    <h3>Order Table Placeholder</h3>
-  </div>`
+  //moduleId: module.id,
+  templateUrl: "orderTable.component.html"
 })
-export class OrderTableComponent { }
+export class OrderTableComponent {
+  includeShipped = false;
+
+  constructor(private repository: OrderRepository) { }
+
+  getOrders(): Order[] {
+    return this.repository.getOrders()
+      .filter(o => this.includeShipped || !o.shipped);
+  }
+
+  markShipped(order: Order) {
+    order.shipped = true;
+    this.repository.updateOrder(order);
+  }
+
+  delete(id: number) {
+    this.repository.deleteOrder(id);
+  }
+}

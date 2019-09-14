dd0f381aea888d8d942d834af9b4284dbf6ab023
Sergey Iryupin <biblelamp@yandex.ru>
2019-09-14 20:37:26 +0200

    deleted unused files

--- Node.js/README.md
+++
@@ -1,5 +0,0 @@
-# JavaScriptExercises | Node.js
-My exercises with Node.js
-
-## Introduction to Node.js
-https://metanit.com/web/nodejs/1.1.php
--- Node.js/helloapp/app.js
+++
@@ -1,8 +0,0 @@
-const http = require("http");
-http.createServer(function(request,response){
-
-    response.end("Hello Node.js!");
-
-}).listen(8080, "127.0.0.1",function(){
-    console.log("Server has started listening on port requests 8080");
-});

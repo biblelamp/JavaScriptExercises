8037bed320ee8cc6645240ea4fade901bd9ea671
Sergey Iryupin <biblelamp@yandex.ru>
2020-01-06 12:44:04 +0100

    press buttons

---
+++ microbit.com/press_buttons.js
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Happy)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Sad)
+})
+basic.forever(function () {
+	
+})

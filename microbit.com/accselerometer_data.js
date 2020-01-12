0a4d0c1d6b1779b8659f2deb6de3dfd0d24937b8
Sergey Iryupin <biblelamp@yandex.ru>
2020-01-12 18:06:33 +0100

    accselerometer data

---
+++ microbit.com/accselerometer_data.js
@@ -0,0 +1,5 @@
+basic.forever(function () {
+    basic.showString("x:" + input.acceleration(Dimension.X))
+    basic.showString("y:" + input.acceleration(Dimension.Y))
+    basic.showString("z:" + input.acceleration(Dimension.Z))
+})

d4595f17a0ba34afadaa790f973db92fc181a660
Sergey Iryupin <biblelamp@yandex.ru>
2020-01-15 18:20:43 +0100

    turtle example

---
+++ microbit.com/turtle.js
@@ -0,0 +1,9 @@
+turtle.home()
+turtle.pen(TurtlePenMode.Down)
+for (let index = 0; index < 2; index++) {
+    turtle.forward(1)
+}
+turtle.turnLeft()
+for (let index = 0; index < 2; index++) {
+    turtle.forward(1)
+}

f75270adb06635f4f27cd70768df894804bf574d
Sergey Iryupin <biblelamp@yandex.ru>
2020-08-11 15:10:18 +0200

    simple app

--- React/app/src/index.js
+++ React/app/src/index.js
@@ -1,1 +1,24 @@
+import React from 'react'
+import {render} from 'react-dom'
 
+function Article() {
+    const body = <section>body</section>
+    return (
+        <div className='hello' style={{color: 'red'}}>
+            <h2>title</h2>
+            {body}
+            <h3>creation date: {(new Date()).toDateString()}</h3>
+        </div>
+    )
+}
+
+function App() {
+    return (
+        <div>
+            <h1>App name</h1>
+            <Article />
+        </div>
+    )
+}
+
+render(<App/>, document.getElementById('root'))

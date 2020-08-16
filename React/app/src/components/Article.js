81fb68213479b76f99efabe0149b34602ac5ea2a
Sergey Iryupin <biblelamp@yandex.ru>
2020-08-16 10:56:40 +0200

    using class

--- React/app/src/components/Article.js
+++ React/app/src/components/Article.js
@@ -1,19 +1,21 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-function Article(props) {
-    const {article} = props
-    console.log('---', props)
-    const body = <section>{article.text}</section>
-    return (
-        <div>
-            <h2>
-                {article.title}
-                <button onClick={handleClick}>close</button>
-            </h2>
-            {body}
-            <h3>creation date: {(new Date(article.date)).toDateString()}</h3>
-        </div>
-    )
+class Article extends Component {
+    render() {
+        const {article} = this.props
+        console.log('---', this.props)
+        const body = <section>{article.text}</section>
+        return (
+            <div>
+                <h2>
+                    {article.title}
+                    <button onClick={handleClick}>close</button>
+                </h2>
+                {body}
+                <h3>creation date: {(new Date(article.date)).toDateString()}</h3>
+            </div>
+        )
+    }
 }
 
 function handleClick() {

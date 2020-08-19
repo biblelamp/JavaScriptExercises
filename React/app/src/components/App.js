d08de8262238301ab662977ee82149587c8928c7
Sergey Iryupin <biblelamp@yandex.ru>
2020-08-19 16:00:26 +0200

    using bootstrap css

--- React/app/src/components/App.js
+++ React/app/src/components/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import ArticleList from './ArticleList'
 import articles from '../fixtures'
+import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
     return (
-        <div>
-            <h1>App name</h1>
-            <ArticleList articles={articles} />
+        <div className="container">
+            <div className="jumbotron">
+                <h1 className="display-2">App name</h1>
+                <ArticleList articles={articles} />
+            </div>
         </div>
     )
 }

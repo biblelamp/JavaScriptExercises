c6b05509061effa5dfaf139c9bc75e8b7a37a6ab
Sergey Iryupin <biblelamp@yandex.ru>
2020-08-21 08:04:05 +0200

    change the order of displaying articles

--- React/app/src/components/App.js
+++ React/app/src/components/App.js
@@ -1,17 +1,31 @@
-import React from 'react'
+import React, {Component} from 'react'
 import ArticleList from './ArticleList'
 import articles from '../fixtures'
 import 'bootstrap/dist/css/bootstrap.css'
 
-function App() {
-    return (
-        <div className="container">
-            <div className="jumbotron">
-                <h1 className="display-2">App name</h1>
-                <ArticleList articles={articles} />
+class App extends Component {
+    state = {
+        reverted: false
+    }
+    render() {
+        return (
+            <div className="container">
+                <div className="jumbotron">
+                    <h1 className="display-3">
+                        App name
+                        <button onClick={this.revert} className="btn">Revert</button>
+                    </h1>
+                    <ArticleList articles={this.state.reverted ?  articles.reverse() : articles} />
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
+
+    revert = () => {
+        this.setState({
+            reverted: !this.state.reverted
+        })
+    }
 }
 
 export default App

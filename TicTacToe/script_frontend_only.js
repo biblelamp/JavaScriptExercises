a82e82f8ffa8a98210bd1cf1b75689e655d41f15
Sergey Iryupin <biblelamp@yandex.ru>
2021-12-02 14:44:59 +0100

    tic tac toe frontend

--- TicTacToe/script_frontend_only.js
+++ TicTacToe/script_frontend_only.js
@@ -11,90 +11,42 @@ const currentPlayerTurn = (player) => `It's ${player}'s turn`;
 const serverURL = "http://localhost:8080";
 const humanPlayer = "X";
 const aiPlayer = "O";
-let gameTable;
-let gameActive;
-
-const winConditions = [
-    [0, 1, 2], [3, 4, 5], [6, 7, 8], // horizontals
-    [0, 3, 6], [1, 4, 7], [2, 5, 8], // verticals
-    [0, 4, 8], [2, 4, 6] // diagonals
-];
+let backendResponse;
+let index;
 
 restartGame();
 
 function restartGame() {
-    gameActive = true;
-    gameTable = ["", "", "", "", "", "", "", "", ""];
     statusDisplay.innerHTML = currentPlayerTurn(humanPlayer);
     document.querySelectorAll('.cell').forEach(cell => cell.innerHTML = "");
-    sendGetRequestToBackend(serverURL + "/init");
+    httpRequestToBackend(serverURL + "/init");
 }
 
 function clickCell(clickedCellEvent) {
     const clickedCell = clickedCellEvent.target;
     const clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index'));
 
-
-    if (gameTable[clickedCellIndex] === "" && gameActive) {
-        sendGetRequestToBackend(serverURL + "/action/human?x=" + clickedCellIndex);
-
-        gameTable[clickedCellIndex] = humanPlayer;
+    httpRequestToBackend(serverURL + "/action/human?X=" + clickedCellIndex);
+    if (index !== -1) {
         clickedCell.innerHTML = humanPlayer;
-        isWinOrDraw();
-    }
 
-    if (gameActive) {
-        turnPlayerAI();
-        isWinOrDraw();
-    }
-}
+        httpRequestToBackend(serverURL + "/status");
+        if (backendResponse === "active") {
+            httpRequestToBackend(serverURL + "/action/ai");
+            if (index !== -1) {
+                let cell = findCellByIndex(index);
+                cell.innerHTML = aiPlayer;
 
-function turnPlayerAI() {
-    let cellIndex;
-    do {
-        cellIndex = getRandomIntInclusive(0, 8);
-    } while (gameTable[cellIndex] !== "");
-
-    sendGetRequestToBackend(serverURL + "/action/ai?o=" + cellIndex);
-
-    gameTable[cellIndex] = aiPlayer;
-    let cell = findCellByIndex(cellIndex);
-    cell.innerHTML = aiPlayer;
-}
-
-function isWinOrDraw() {
-    console.log("isWinOrDraw", gameTable);
-
-    let signWon;
-    let flagWon = false;
-    for (let i = 0; i < winConditions.length; i++) {
-        const winCondition = winConditions[i];
-        let a = gameTable[winCondition[0]];
-        let b = gameTable[winCondition[1]];
-        let c = gameTable[winCondition[2]];
-        if (a === '' || b === '' || c === '') {
-            continue;
+                httpRequestToBackend(serverURL + "/status");
+            }
         }
-        if (a === b && b === c) {
-            flagWon = true;
-            signWon = a;
-            break;
+
+        if (backendResponse === "draw") {
+            statusDisplay.innerHTML = drawMessage();
         }
-    }
-
-    sendGetRequestToBackend(serverURL + "/status/win");
-
-    if (flagWon) {
-        statusDisplay.innerHTML = winMessage(signWon);
-        gameActive = false;
-        return;
-    }
-
-    let flagDraw = !gameTable.includes("");
-    if (flagDraw) {
-        statusDisplay.innerHTML = drawMessage();
-        gameActive = false;
-        return;
+        if (backendResponse === humanPlayer || backendResponse === aiPlayer) {
+            statusDisplay.innerHTML = winMessage(backendResponse);
+        }
     }
 }
 
@@ -109,20 +61,16 @@ function findCellByIndex(cellIndex) {
     return findCell;
 }
 
-function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function sendGetRequestToBackend(url) {
+function httpRequestToBackend(url) {
+    // https://learn.javascript.ru/xmlhttprequest
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
         if (xhr.readyState == XMLHttpRequest.DONE) {
-            console.log(xhr.responseText);
-            return xhr.responseText
+            console.log(url, xhr.responseText);
+            backendResponse = xhr.responseText;
+            index = parseInt(backendResponse);
         }
     }
-    xhr.open('GET', url, true);
-    xhr.send(null);
+    xhr.open('GET', url, false); // false = sync request
+    xhr.send();
 }

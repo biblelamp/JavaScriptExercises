3fae5e374460893ffefd549f17672eba91c934f6
Sergey Iryupin <biblelamp@yandex.ru>
2023-10-01 06:52:11 +0200

    event frontend: read/add/update/delete

--- Event/common-event.js
+++ Event/common-event.js
@@ -1,8 +1,3 @@
-const postForm = document.getElementById("post-event");
-
-// define submit button handler function
-postForm.addEventListener("submit", handleFormSubmit);
-
 /**
  * Event handler for a form submit event.
  *
@@ -22,10 +17,13 @@ async function handleFormSubmit(event) {
     // get url action from form
     const url = form.action;
 
+    // get method from form
+    const method = form.target;
+
     try {
         const formData = new FormData(form);
-        const responseData = await postFormDataAsJson({url, formData});
-        console.log({responseData});
+        const responseData = await postFormDataAsJson({url, method, formData});
+        console.log(responseData);
 
         window.location.replace("index.html");
     } catch (error) {
@@ -37,22 +35,24 @@ async function handleFormSubmit(event) {
  * Helper function for POSTing data as JSON with fetch.
  *
  * @param {string} options.url - URL to POST data to
+ * @param {string} optiond.method - POST/PUT/DELETE
  * @param {FormData} options.formData - `FormData` instance
  * @return {Object} - Response body from URL that was POSTed to
  */
-async function postFormDataAsJson({url, formData}) {
+async function postFormDataAsJson({url, method, formData}) {
 
     const plainFormData = Object.fromEntries(formData.entries());
     const formDataJsonString = JSON.stringify(plainFormData);
 
     const fetchOptions = {
-        method: "POST",
+        method: method,
         headers: {
             "Content-Type": "application/json",
             "Accept": "application/json"
         },
         body: formDataJsonString,
     };
+    console.log(fetchOptions);
 
     const response = await fetch(url, fetchOptions);
 
--- Event/index.js
+++ Event/index.js
@@ -1,5 +1,11 @@
+// server url
+const SRV_NAME = 'http://34.68.240.57'; // 34.68.240.57;
+
+// server port
+const SRV_PORT = 8080;
+
 // calling the async function
-getapi('http://localhost:8080/event');
+getapi(SRV_NAME + ':' + SRV_PORT + '/event/all');
 
 // defining async function
 async function getapi(url) {
@@ -16,6 +22,7 @@ async function getapi(url) {
             <th>Id</th>
             <th>Event Name</th>
             <th>City</th>
+            <th colspan="2">Actions</th>
         </tr>`;
 
     // loop to access all rows
@@ -26,9 +33,36 @@ async function getapi(url) {
             <td>${item.id}</td>
             <td>${item.name}</td>
             <td>${item.city}</td>
+            <td><a href="update-event.html?id=${item.id}&name=${item.name}&city=${item.city}">edit</a></td>
+            <td>
+                <button onclick="deleteEvent('${SRV_NAME}:${SRV_PORT}/event/delete/${item.id}')">
+                    delete
+                </button>
+            </td>
         </tr>`;
     }
 
     // setting innerHTML as tab variable
     document.getElementById("table").innerHTML = table;
+}
+
+// delete event
+async function deleteEvent(url) {
+
+    const fetchOptions = {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        }
+    };
+
+    const response = await fetch(url, fetchOptions);
+
+    if (!response.ok) {
+        const errorMessage = await response.text();
+        throw new Error(errorMessage);
+    }
+
+    window.location.replace("index.html");
 }
